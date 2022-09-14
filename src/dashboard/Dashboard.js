a6e5af819dcfa9cd66eda54d18bb8c12f0f8cdc1
RickChen1220 <zx12201993@gmail.com>
2022-09-14 16:50:34 +0800

    delete useless file

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -3,15 +3,8 @@ import DashboardLayout from "layouts/DashboardLayout";
 
 export default function Dashboard() {
   return (
-<<<<<<< HEAD
-    <Box sx={{ flexGrow: 1 }}>
-      <Navigation />
-      <h1>Agent: 布加拉提</h1>
-    </Box>
-=======
     <DashboardLayout>
       <h1>This is dashboard for goat.</h1>
     </DashboardLayout>
->>>>>>> 10b3b11 (use map in List)
   );
 }
